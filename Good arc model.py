ee53e43f70a27946489224c5d867e873051b9537
ZeustheGreek10 <pkosztas@gmail.com>
2026-04-30 14:12:32 +0200

    alksj

--- Good arc model.py
+++ Good arc model.py
@@ -25,12 +25,15 @@ aileron_df, _ = data_grabber(flight_name, data_type, f"DeltaAil_subfile_{i}", ge
 airspeed_df, _ = data_grabber(flight_name, data_type, f"VTrue_subfile_{i}", gettick=False)
 pressure_df, _ = data_grabber(flight_name, data_type, f"StaticPres_subfile_{i}", gettick=False)
 temperature_df, _ = data_grabber(flight_name, data_type, f"StaticTemp_subfile_{i}", gettick=False)
+alpha_df, _ = data_grabber(flight_name, data_type, f"Alpha_subfile_{i}", gettick=False)
 
 if time_df is None or input_df is None:
     raise FileNotFoundError("Data grabber failed to find the files. Your shit is wrong !")
 
 
 aileron_df -= np.mean(aileron_df[-100:])
+#drum_df -= np.mean(drum_df[-100:])
+input_df -= np.mean(input_df[-100:])
 
 
 time = time_df['value'].values
@@ -43,10 +46,14 @@ pressure = pressure_df['value'].values
 temperature = temperature_df['value'].values
 temperature = temperature.copy()
 temperature += 273.15
+alpha = alpha_df['value'].values
 
-input_df = savgol_filter(input_df['value'].values, window_length=130, polyorder=2)
-drum_df = savgol_filter(drum_df['value'].values, window_length=130, polyorder=2)
-aileron_df = savgol_filter(aileron_df['value'].values, window_length=130, polyorder=2)
+
+input_command = savgol_filter(input_df['value'].values, window_length=101, polyorder=2)
+delta_drum = savgol_filter(drum_df['value'].values, window_length=101, polyorder=2)
+delta_aileron = savgol_filter(aileron_df['value'].values, window_length=101, polyorder=2)
+
+delta_drum = -delta_drum
 
 print(time_seconds[:5])
 # ============================================
@@ -61,10 +68,18 @@ q = 0.5 * rho * airspeed**2              # dynamic pressure [Pa]
 # 3. DEFINE DYNAMICS (4 states)
 # ============================================
 fixed = False
+delay = False
+detrended = True
+test = True
+start_idx_eval = 0
+end_idx_eval = 160000
+
+
 def actuator_with_linkage(t, x, u, params):
     theta_d, theta_d_dot, theta_a, theta_a_dot = x
     current = u[0]
-    q_val = u[1]  # not used now (constant aerodynamic terms)
+    q_val = u[1]
+    alpha_val = u[2]
 
     J_d = params['J_drum']
     J_a = params['J_aileron']
@@ -74,7 +89,7 @@ def actuator_with_linkage(t, x, u, params):
 
     I_dead = 0.02
     current_eff = np.sign(current) * np.maximum(np.abs(current) - I_dead, 0.0)
-    tau_m = params['torque_gain'] * current_eff    
+    tau_m = -params['torque_gain'] * current_eff    
 
     if fixed:
         K_a = 1e-3 * q_val
@@ -84,11 +99,11 @@ def actuator_with_linkage(t, x, u, params):
         K_a = params['aero_stiffness'] * q_val
         B_a = params['aero_damping'] * q_val
 
-    rel = theta_d + theta_a 
-    rel_dot = theta_d_dot + theta_a_dot
+    rel = theta_d - theta_a 
+    rel_dot = theta_d_dot - theta_a_dot
 
     theta_d_ddot = (tau_m - K_s*rel - B_s*rel_dot - B_d*theta_d_dot) / J_d
-    theta_a_ddot = (- K_s*rel - B_s*rel_dot - K_a*theta_a - B_a*theta_a_dot) / J_a
+    theta_a_ddot = (K_s*rel + B_s*rel_dot - K_a*theta_a - B_a*theta_a_dot) / J_a
 
     # Return as a flat 1D array
     return np.hstack([theta_d_dot, theta_d_ddot, theta_a_dot, theta_a_ddot])
@@ -99,15 +114,17 @@ def actuator_with_linkage(t, x, u, params):
 # ============================================
 
 def measure_positions(t, x, u, params):
-    return np.hstack([x[0], x[2]])   # [drum, aileron]
-
+    return np.hstack([
+        x[0] + params['drum_bias'],
+        x[2] + params['aileron_bias']
+    ])
 
 # --- Test the dynamics and measurement ---
 sample_t = time_seconds[0]
 sample_x = np.array([0.0, 0.0, 0.0, 0.0])
-sample_u = np.array([input_command[0], q[0]])
+sample_u = np.array([input_command[0], q[0], alpha[0]])
 sample_params = {
-    'J_drum': 0.01, 'J_aileron': 0.1, 'link_stiffness': 100.0,
+    'drum_bias': 0, 'aileron_bias': 0, 'J_drum': 0.01, 'J_aileron': 0.1, 'link_stiffness': 100.0,
     'link_damping': 1.0, 'drum_friction': 0.1, 'torque_gain': 1.0,
     'aero_stiffness': 0.05, 'aero_damping': 0.02
 }
@@ -136,7 +153,7 @@ ekf = arc.observers.ExtendedKalmanFilter(dyn_discrete, measure_positions, Q, R)
 # ============================================
 # 6. PREPARE THE DATA
 # ============================================
-us = np.stack([input_command, q], axis=0)        # shape (2, N)
+us = np.stack([input_command, q, alpha], axis=0)        # shape (2, N)
 ys = np.array([delta_drum, delta_aileron])       # shape (2, N)
 
 time = np.ascontiguousarray(time_seconds, dtype=np.float64)
@@ -161,11 +178,13 @@ if fixed:
         }
 else:
     params_guess = {
+        'drum_bias': delta_drum[start_idx_eval],
+        'aileron_bias': delta_aileron[start_idx_eval],
         'J_aileron': 3.1416e-02,
         'J_drum': 1.0455e-01,
         'aero_damping': 2.7328e-04,
         'aero_stiffness': 2.9755e-03,
-        'drum_friction': 6.0892e-06,
+        'drum_friction': 5e-02,
         'link_damping': 2.1163e+00,
         'link_stiffness': 3.9388e+01,
         'torque_gain': 7.7989e-01,
@@ -176,33 +195,36 @@ else:
 # ============================================
 # 8. RUN THE IDENTIFICATION
 # ============================================
-event_idx = np.argmax(np.abs(input_command - np.mean(input_command)))
-start_idx = max(0, event_idx - 1500)
+activity = np.abs(np.gradient(delta_drum)) + np.abs(np.gradient(delta_aileron))
+event_idx = np.argmax(activity)
+start_idx = max(0, event_idx - 1000)
 N_sub = 2000
 
 time_sub = time[start_idx:start_idx + N_sub]
 
 q_sub = q[start_idx:start_idx + N_sub]
 ys_sub = ys[:, start_idx:start_idx + N_sub].copy()
-
-ys_sub[0, :] -= ys_sub[0, 0]
-ys_sub[1, :] -= ys_sub[1, 0]
-
 input_sub = input_command[start_idx:start_idx + N_sub]
-input_sub_ref = input_sub - input_sub[0]
+alpha_sub = alpha[start_idx:start_idx + N_sub]
 
 
-delay = False
+if detrended:
+    ys_sub[0, :] -= ys_sub[0, 0]
+    ys_sub[1, :] -= ys_sub[1, 0]
+
+input_sub = input_sub - input_sub[0]
+
+
 if delay:
     delay_samples = 10
 
-    input_sub_delayed = np.empty_like(input_sub_ref)
-    input_sub_delayed[:delay_samples] = input_sub_ref[0]
-    input_sub_delayed[delay_samples:] = input_sub_ref[:-delay_samples]
+    input_sub_delayed = np.empty_like(input_sub)
+    input_sub_delayed[:delay_samples] = input_sub[0]
+    input_sub_delayed[delay_samples:] = input_sub[:-delay_samples]
 
-    us_sub = np.stack([input_sub_delayed, q_sub], axis=0)
+    us_sub = np.stack([input_sub_delayed, q_sub, alpha_sub], axis=0)
 else:
-    us_sub = np.stack([input_sub_ref, q_sub], axis=0)
+    us_sub = np.stack([input_sub, q_sub, alpha_sub], axis=0)
 
 
 data_sub = arc.sysid.Timeseries(ts=time_sub, us=us_sub, ys=ys_sub)
@@ -216,7 +238,7 @@ options = {'max_nfev': 500, 'ftol': 1e-6, 'xtol': 1e-6}
 if fixed:
     lower_bounds = {
         'J_drum': 8e-3,
-        'J_aileron': 8e-2,
+        'J_aileron': 8e-3,
         'link_stiffness': 50.0,
         'link_damping': 0.2,
         'drum_friction': 0.01,
@@ -236,6 +258,8 @@ if fixed:
 
 else:
     lower_bounds = {
+        'drum_bias': -1e-1,
+        'aileron_bias': -1e-1,
         'J_aileron': 1e-2,
         'J_drum': 8e-2,
         'aero_damping': 9e-05,
@@ -247,11 +271,13 @@ else:
         }
 
     upper_bounds = {
+        'drum_bias': 1e-1,
+        'aileron_bias': 1e-1,
         'J_aileron': 9e-2,
         'J_drum': 1,
         'aero_damping': 3e-03,
         'aero_stiffness': 2e-02,
-        'drum_friction': 1e-4,
+        'drum_friction': 1e-0,
         'link_damping': 3e0,
         'link_stiffness': 5e01,
         'torque_gain': 1e0,
@@ -308,7 +334,7 @@ print("x1_euler =", x1_euler)
 # ============================================
 # 9. RESULTS
 # ============================================
-test = True
+
 
 print("\n=== Estimated Parameters ===")
 for key, val in result.p.items():
@@ -431,31 +457,30 @@ print(f"\nFit for drum: {fit_drum:.1f}%")
 print(f"Fit for aileron: {fit_ail:.1f}%")
 
 
-# shortening the simulation interval
-
-i = 1 * 1000 # start time in ticks
-j = 0 * 1000 # ticks before end of dataset
 
 
+def simulate_dataset(params, time_vec, u_mat, x0, start_idx, end_idx):
+    if end_idx is None:
+        end_idx = u_mat.shape[1]
 
-def simulate_dataset(params, time_vec, u_mat, x0):
-    n_steps = u_mat.shape[1]
+    time_eval = time_vec[start_idx:end_idx]
+    u_eval = u_mat[:, start_idx:end_idx]
+
+    n_steps = u_eval.shape[1]
     states = np.zeros((4, n_steps))
     outputs = np.zeros((2, n_steps))
 
     x = x0.copy()
     states[:, 0] = x
-    outputs[:, 0] = measure_positions(time_vec[0], x, u_mat[:, 0], params)
+    outputs[:, 0] = measure_positions(time_eval[0], x, u_eval[:, 0], params)
 
-
-
-    for k in range(i, n_steps-j):
-        t_cur = time_vec[k - 1]
-        x = dyn_discrete(t_cur, x, u_mat[:, k - 1], params)
+    for k in range(1, n_steps):
+        t_cur = time_eval[k - 1]
+        x = dyn_discrete(t_cur, x, u_eval[:, k - 1], params)
         states[:, k] = x
-        outputs[:, k] = measure_positions(time_vec[k], x, u_mat[:, k], params)
+        outputs[:, k] = measure_positions(time_eval[k], x, u_eval[:, k], params)
 
-    return states, outputs
+    return time_eval, states, outputs
 
 
 def fit_percent(y, yhat):
@@ -465,16 +490,20 @@ def fit_percent(y, yhat):
     return 100 * (1 - np.linalg.norm(y - yhat) / denom)
 
 
-def evaluate_params_on_full_dataset(params, use_measured_initial_position=True, make_plots=True):
+
+
+def evaluate_params_on_full_dataset(params, start_idx, end_idx, use_measured_initial_position=True, make_plots=True):
     # Full measured outputs
     ys_full = np.vstack([delta_drum, delta_aileron])
+
+
 
     # Better than starting from zero for full-data validation
     if use_measured_initial_position:
         x0_full = np.array([
-            delta_drum[0],
+            delta_drum[start_idx],
             0.0,
-            delta_aileron[0],
+            delta_aileron[start_idx],
             0.0
         ], dtype=float)
     else:
@@ -482,10 +511,14 @@ def evaluate_params_on_full_dataset(params, use_measured_initial_position=True, 
 
     # Simulate full dataset with fixed params
     # Use the same input convention as in training
-    input_full_ref = input_command - input_command[0]
-    us_full_eval = np.stack([input_full_ref, q], axis=0)
+    input_full_ref = input_command - input_command[start_idx]
+    us_full_eval = np.stack([input_full_ref, q, alpha], axis=0)
 
-    states_full, outputs_full = simulate_dataset(params, time_seconds, us_full_eval, x0_full)
+    time_eval, states_full, outputs_full = simulate_dataset(
+        params, time_seconds, us_full_eval, x0_full, start_idx, end_idx
+        )
+    
+    ys_full = np.vstack([delta_drum, delta_aileron])[:, start_idx:end_idx]
 
     # Raw fits
     fit_drum_raw = fit_percent(ys_full[0], outputs_full[0])
@@ -506,14 +539,14 @@ def evaluate_params_on_full_dataset(params, use_measured_initial_position=True, 
 
     if make_plots:
         fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(10, 6))
-        ax1.plot(time_seconds, ys_full[0], 'b-', label='Measured drum')
-        ax1.plot(time_seconds, outputs_full[0], 'r--', label='Simulated drum')
+        ax1.plot(time_eval, ys_full[0], 'b-', label='Measured drum')
+        ax1.plot(time_eval, outputs_full[0], 'r--', label='Simulated drum')
         ax1.set_ylabel('Drum position (rad)')
         ax1.legend()
         ax1.grid(True)
 
-        ax2.plot(time_seconds, ys_full[1], 'b-', label='Measured aileron')
-        ax2.plot(time_seconds, outputs_full[1], 'r--', label='Simulated aileron')
+        ax2.plot(time_eval, ys_full[1], 'b-', label='Measured aileron')
+        ax2.plot(time_eval, outputs_full[1], 'r--', label='Simulated aileron')
         ax2.set_ylabel('Aileron position (rad)')
         ax2.set_xlabel('Time (s)')
         ax2.legend()
@@ -522,20 +555,43 @@ def evaluate_params_on_full_dataset(params, use_measured_initial_position=True, 
         plt.show()
 
         fig, (ax3, ax4) = plt.subplots(2, 1, sharex=True, figsize=(10, 6))
-        ax3.plot(time_seconds, ys_full_detr[0], 'b-', label='Measured drum (detrended)')
-        ax3.plot(time_seconds, outputs_full_detr[0], 'r--', label='Simulated drum (detrended)')
+        ax3.plot(time_eval, ys_full_detr[0], 'b-', label='Measured drum (detrended)')
+        ax3.plot(time_eval, outputs_full_detr[0], 'r--', label='Simulated drum (detrended)')
         ax3.set_ylabel('Drum position (rad)')
         ax3.legend()
         ax3.grid(True)
 
-        ax4.plot(time_seconds, ys_full_detr[1], 'b-', label='Measured aileron (detrended)')
-        ax4.plot(time_seconds, outputs_full_detr[1], 'r--', label='Simulated aileron (detrended)')
+        ax4.plot(time_eval, ys_full_detr[1], 'b-', label='Measured aileron (detrended)')
+        ax4.plot(time_eval, outputs_full_detr[1], 'r--', label='Simulated aileron (detrended)')
         ax4.set_ylabel('Aileron position (rad)')
         ax4.set_xlabel('Time (s)')
         ax4.legend()
         ax4.grid(True)
 
         plt.show()
+
+        e_drum = ys_full_detr[0] - outputs_full_detr[0]
+
+        plt.figure(figsize=(10,4))
+        plt.plot(time_eval, e_drum)
+        plt.grid(True)
+        plt.ylabel("Drum error (rad)")
+        plt.xlabel("Time (s)")
+        plt.title("Detrended drum error")
+        plt.show()
+
+        print("Mean drum error:", np.mean(e_drum))
+        print("RMS drum error:", np.sqrt(np.mean(e_drum**2)))
+
+        e_drum = ys_full_detr[0] - outputs_full_detr[0]
+        drum_offset_corr = np.mean(e_drum)
+
+        outputs_full_detr_corr = outputs_full_detr.copy()
+        outputs_full_detr_corr[0] += drum_offset_corr
+
+        fit_drum_detr_corr = fit_percent(ys_full_detr[0], outputs_full_detr_corr[0])
+        print("Corrected detrended drum fit:", fit_drum_detr_corr)
+        print("Applied drum offset correction:", drum_offset_corr)
 
     return {
         'x0_full': x0_full,
@@ -547,4 +603,6 @@ def evaluate_params_on_full_dataset(params, use_measured_initial_position=True, 
         'fit_ail_detr': fit_ail_detr,
     }
 
-full_evaluation_results = evaluate_params_on_full_dataset(params_test, use_measured_initial_position=True, make_plots=True)
+full_evaluation_results = evaluate_params_on_full_dataset(params_test, start_idx_eval, end_idx_eval, use_measured_initial_position=True, make_plots=True)
+
+
